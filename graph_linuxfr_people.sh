3592b5ac32494c3ebb42f25cc32daabd256fd824
Tiwaz <adrien.henriet@gmail.com>
2016-08-20 17:52:09 +0200

    Bugfix for space in name

--- graph_linuxfr_people.sh
+++ graph_linuxfr_people.sh
@@ -3,4 +3,4 @@ if [ $# -ne 2 ]; then
   echo "Usage: $0 [inputfile] [outputfile]"
   exit 1
 fi
-cat $1|cut -d":" -f1|sort| uniq -c|sort -r -n|awk '{print $2,$1}'|gnuplot -p -e "set style data histograms;set style fill solid 1.00 border -1;set xtic rotate by -65;set term png;set output '$2';plot COL=2 '<cat' using COL:xticlabels(1) notitle"
+cat $1|cut -d":" -f1|sort| uniq -c|sort -r -n|sed -e 's/^[[:space:]]*//'|sed 's/ / : /'|gnuplot -p -e "set style data histograms;set style fill solid 1.00 border -1;set datafile separator ':';set xtic rotate by -65;set term png;set output '$2';plot COL=1 '<cat' using COL:xticlabels(2) notitle"
