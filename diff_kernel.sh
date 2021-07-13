735ea5eaaea5111a1da46e9fc8219878068fccb8
Valinor <Valinor@users.noreply.github.com>
2021-07-13 15:37:58 +0200

    Automate graph generation on git Kernel

--- diff_kernel.sh
+++ diff_kernel.sh
@@ -1,19 +1,7 @@
-echo "diff"
-for i in {1..6};do echo -n "rc"$i",";git diff v4.7-rc$i v4.7-rc`expr $i + 1`| wc -l;done
-
-echo "add"
-for i in {1..6};do git diff v4.7-rc$i v4.7-rc`expr $i + 1`| grep "^+"|wc -l;done
-
-echo "delete"
-for i in {1..6};do git diff v4.7-rc$i v4.7-rc`expr $i + 1`| grep "^-"|wc -l;done
-
-echo "LINUX DIFF:"
-
-for i in {0..6};do echo -n "kernel 4."`expr $i + 1`,;git diff v4.$i v4.`expr $i + 1`| wc -l;done
-
-echo "LINUX ADD:"
-for i in {0..6};do echo -n "kernel 4."`expr $i + 1`,;git diff v4.$i v4.`expr $i + 1`| grep "^+"|wc -l;done
-
-
-echo "LINUX SUPP:"
-for i in {0..6};do echo -n "kernel 4."`expr $i + 1`,;git diff v4.$i v4.`expr $i + 1`| grep "^-"|wc -l;done
+base=${1:-v5.8.}
+result=$(echo $base|sed s/[.]/\\\\./g)
+echo """echo -n \$1' ' && git diff --numstat \$1 \$2 |awk 'BEGIN{add=0;supp=0} {add=add+\$1;supp=supp+\$2} END{print add\" -\"supp}'""" > fnn.sh
+chmod +x fnn.sh
+git tag |grep $result[[:digit:]]*$|sort -V|awk 'BEGIN{i=0} {if (i==0) { un=$1;i=1 } else { deux=un;un=$1;printf "%s %s\n",deux,un }}'|xargs -I% ./fnn.sh % > stat.txt
+gnuplot -e "set term png; set xtics rotate by 90 right;set boxwidth 0.5 absolute;set style fill solid border;set output '"$base".png';plot 'stat.txt' u 0:3:xtic(1) with boxes title 'Add line','' u 0:4:xtic(1) with boxes title 'Delete lines'"
+rm fnn.sh
